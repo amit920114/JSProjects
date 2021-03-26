5eadc2aa48ce9c0d2d6b358606711a0066d59c61
amit <amit920114kumar@gmail.com>
2021-03-26 15:12:40 +0530

    Note Pad

---
+++ Note Pad/js/app.js
@@ -0,0 +1,49 @@
+console.log("First Project");
+
+let addBtn =  document.getElementById('addBtn');
+addBtn.addEventListener("click", function(e){
+    let addTxt = document.getElementById('addTxt');
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }else{
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    // console.log(addTxt.value)
+    // console.log("amit")
+    showNotes();
+})
+ function showNotes(){
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }else{
+        notesObj = JSON.parse(notes);
+    }
+    // console.log(notes)
+ 
+ let html = "";
+ notesObj.forEach(function(element, index){
+     html+= `
+     <div class="card my-2 mx-2" style="width: 18rem;">
+     
+     <div class="card-body">
+       <h5 class="card-title">Note ${index +1}</h5>
+       <p class="card-text">${element}</p>
+    
+     </div>
+   </div>
+
+             `
+     
+ });
+ let notesElm = document.getElementById("notes");
+ if(notesObj.length != 0){
+     notesElm.innerHTML = html;
+ } else{
+     notesElm.innerText = ` Nothing to show`
+ }
+}
